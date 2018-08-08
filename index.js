ac74a7f94a72412444bb67ba453a3ffdfc523c53
lstern22 <lstern220@gmail.com>
2018-08-08 14:15:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
 const companyName= 'Scuber';
 var companyName= 'Scuber';
-let mostProfitableNeighborhood= 'chelsea';
+var mostProfitableNeighborhood== 'chelsea';
