ae46ad68cc6fec819b962ecfafd36814ccc96d38
lstern22 <lstern220@gmail.com>
2018-08-08 14:19:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
 const companyName= 'Scuber';
-var mostProfitableNeighborhood= 'chelsea';
+var mostProfitableNeighborhood= 'Chelsea';
 var companyCeo = 'Susan Smith';
